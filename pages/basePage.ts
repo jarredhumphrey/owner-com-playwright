957074d4fb0330b429bdbb5720f5a3a6395beb83
Jarred Humphrey <jarred.humphrey@willowtreeapps.com>
2025-09-10 11:19:09 -0400

    initialized playwright and added tab navigation tests

---
+++ pages/basePage.ts
@@ -0,0 +1,30 @@
+import { Locator, Page } from "@playwright/test";
+
+export class BasePage {
+  readonly page: Page;
+  readonly navBar: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.navBar = page.getByRole("navigation");
+  }
+
+  async navigateToTab(tabName: string): Promise<void> {
+    await this.navBar.getByRole("link", { name: tabName }).click();
+  }
+
+  async hoverTab(tabName: string): Promise<void> {
+    await this.navBar.getByRole("button", { name: tabName }).hover();
+  }
+
+  async navigateToTabSubLink(tabName: string, linkName: string): Promise<void> {
+    const tabButton = await this.page.getByRole("button", { name: tabName });
+    const subLinksList = await this.page
+      .locator(".nav_menu-dropdown")
+      .filter({ has: tabButton })
+      .locator("ul");
+
+    await this.hoverTab(tabName);
+    await subLinksList.getByRole("link", { name: linkName }).click();
+  }
+}
---
+++ tests/example.spec.ts
@@ -0,0 +1,86 @@
+import { test, expect } from "@playwright/test";
+import { BasePage } from "../pages/basePage";
+
+test.describe("Owner.com navigation", () => {
+  let basePage: BasePage;
+
+  test.beforeEach(async ({ page }) => {
+    basePage = new BasePage(page);
+    await page.goto("/");
+  });
+
+  test("Navigate between product links", async ({ page }) => {
+    await basePage.navigateToTabSubLink("Product", "Website Builder");
+    expect.soft(page).toHaveURL("/restaurant-website-ai");
+
+    await basePage.navigateToTabSubLink("Product", "Automated Marketing");
+    expect.soft(page).toHaveURL("/automatic-marketing");
+
+    await basePage.navigateToTabSubLink("Product", "Zero-Commission Delivery");
+    expect.soft(page).toHaveURL("/delivery");
+
+    await basePage.navigateToTabSubLink("Product", "Custom Mobile App");
+    expect.soft(page).toHaveURL("/branded-apps");
+
+    await basePage.navigateToTabSubLink("Product", "Online Ordering");
+    expect.soft(page).toHaveURL("/online-ordering");
+
+    await basePage.navigateToTabSubLink("Product", "Loyalty Program");
+    expect(page).toHaveURL("/loyalty-rewards");
+  });
+
+  test("Navigate to pricing tab", async ({ page }) => {
+    await basePage.navigateToTab("Pricing");
+    expect(page).toHaveURL("/pricing");
+  });
+
+  test("Navigate to how it works tab", async ({ page }) => {
+    await basePage.navigateToTab("How it works");
+    expect(page).toHaveURL("/how-owner-works");
+  });
+
+  test("Navigate between company links", async ({ page }) => {
+    await basePage.navigateToTabSubLink("Company", "Our Story");
+    expect.soft(page).toHaveURL("/our-story");
+
+    await basePage.navigateToTabSubLink("Company", "Careers");
+    expect.soft(page).toHaveURL("/careers");
+
+    await basePage.navigateToTabSubLink("Company", "Leadership");
+    expect.soft(page).toHaveURL("/leadership");
+
+    await basePage.navigateToTabSubLink("Company", "Press");
+    expect.soft(page).toHaveURL("/press");
+
+    await basePage.navigateToTabSubLink("Company", "Reviews");
+    expect.soft(page).toHaveURL("/reviews");
+
+    await basePage.navigateToTabSubLink("Company", "Product Releases");
+    expect.soft(page).toHaveURL("/releases");
+
+    await basePage.navigateToTabSubLink("Company", "Partner with Owner");
+    expect(page).toHaveURL("/partner-request");
+  });
+
+  test("Navigate between resources links", async ({ page }) => {
+    const newTabPromise = page.waitForEvent("popup");
+
+    await basePage.navigateToTabSubLink("Resources", "Blog");
+    expect.soft(page).toHaveURL("/blog");
+
+    await basePage.navigateToTabSubLink("Resources", "Help Center");
+    const newTab = await newTabPromise;
+    await newTab.waitForLoadState();
+    expect.soft(newTab.url()).toContain("help.owner.com");
+    await newTab.close();
+
+    await basePage.navigateToTabSubLink("Resources", "Case Studies");
+    expect.soft(page).toHaveURL("/case-studies");
+
+    await basePage.navigateToTabSubLink("Resources", "Support Center");
+    expect.soft(page).toHaveURL("/support");
+
+    await basePage.navigateToTabSubLink("Resources", "Compare Owner");
+    expect(page).toHaveURL("/comparison");
+  });
+});
